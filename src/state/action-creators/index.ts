0766e0776acbf58199459525b316d6faf545253e
Ronald Arias <ariasron@hotmail.com>
2022-06-28 12:26:04 -0500

    creates action-creators and store

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,33 @@
+// action-creators are functions to dispatch actions
+
+import { ActionType } from '../action-types'
+import { Dispatch } from 'redux'
+import { Action } from '../actions'
+
+const depositMoney = (amount: number) => {
+  return (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.DEPOSIT,
+      payload: amount,
+    })
+  }
+}
+
+const withdrawMoney = (amount: number) => {
+  return (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.WITHDRAW,
+      payload: amount,
+    })
+  }
+}
+
+const bankrupt = () => {
+  return (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.BANKRUPT,
+    })
+  }
+}
+
+export { depositMoney, withdrawMoney, bankrupt }
